7c2ac2eb23f3db391c5c25041611e99b9e6cd6b9
Engraya <ahmadyakubuaya@gmail.com>
2024-04-21 19:03:15 +0100

    Initial UI Commit

--- src/app/components/Demo.tsx
+++ src/app/components/Demo.tsx
@@ -1,9 +1,35 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <>
-        <div className="relative">
+<div>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <div className="bg-[#f8f9ff] font-[sans-serif] text-[#333] text-[15px]">
+    <header className="py-4 px-4 sm:px-10 z-50 min-h-[70px]">
+      <div className="relative flex flex-wrap items-center gap-4">
+        <a href="javascript:void(0)"><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className="w-36" />
+        </a>
+        <div className="flex ml-auto lg:order-1">
+          <button className="px-6 py-3 rounded-xl text-white bg-cyan-900 transition-all hover:bg-cyan-800">Login</button>
+          <button id="toggle" className="lg:hidden ml-7">
+            <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+              <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+            </svg>
+          </button>
+        </div>
+        <ul id="collapseMenu" className="lg:!flex lg:ml-12 lg:space-x-6 max-lg:space-y-6 max-lg:hidden max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[250px] max-lg:top-0 max-lg:left-0 max-lg:p-4 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto max-lg:z-50">
+          <li className="max-lg:border-b max-lg:py-2 px-3">
+            <a href="javascript:void(0)" className="lg:hover:text-blue-600 text-blue-600 block font-bold transition-all">Home</a>
+          </li>
+          <li className="max-lg:border-b max-lg:py-2 px-3"><a href="javascript:void(0)" className="lg:hover:text-blue-600 block font-bold transition-all">Team</a>
+          </li>
+          <li className="max-lg:border-b max-lg:py-2 px-3"><a href="javascript:void(0)" className="lg:hover:text-blue-600 block font-bold transition-all">Feature</a>
+          </li>
+          <li className="max-lg:border-b max-lg:py-2 px-3"><a href="javascript:void(0)" className="lg:hover:text-blue-600 block font-bold transition-all">Blog</a>
+          </li>
+          <li className="max-lg:border-b max-lg:py-2 px-3"><a href="javascript:void(0)" className="lg:hover:text-blue-600 block font-bold transition-all">About</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+    <div className="relative">
       <div className="px-4 sm:px-10">
         <div className="mt-16 max-w-4xl mx-auto text-center relative z-10">
           <h1 className="md:text-6xl text-4xl font-extrabold mb-6 md:!leading-[75px]">Build Landing Pages with Typeform
@@ -525,6 +551,87 @@ export default function Home() {
         </div>
       </div>
     </div>
-    </>
-  );
-}
+    <footer className="bg-cyan-900 px-4 sm:px-10 py-12 mt-32">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div>
+          <h4 className="text-white font-extrabold text-xl mb-6">Quick Links</h4>
+          <ul className="space-y-4">
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Our Story</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Newsroom</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Careers</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Blog</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="text-white font-extrabold text-xl mb-6">Services</h4>
+          <ul className="space-y-4">
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Web Development</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Testing Automation</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">AWS Development
+                Services</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Mobile App
+                Development</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="text-white font-extrabold text-xl mb-6">Platforms</h4>
+          <ul className="space-y-4">
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Hubspot</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Marketo Integration
+                Services</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Marketing Glossary</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">UIPath</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4 className="text-white font-extrabold text-xl mb-6">Company</h4>
+          <ul className="space-y-4">
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Accessibility</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">About</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Contact</a>
+            </li>
+            <li>
+              <a href="javascript:void(0)" className="hover:text-white text-gray-300 transition-all">Learn more</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="border-t text-center border-gray-400 pt-8 mt-8">
+        <p className="text-gray-300">
+          Copyright © 2023
+          <a href="https://readymadeui.com/" target="_blank" className="hover:underline mx-1">ReadymadeUI</a>
+          All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  </div>
+</div>
